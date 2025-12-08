67f7dd13a6bfbfb73339e6f1a7a0529746da3ea0
Rinatsu <Rinatsuko@proton.me>
2025-12-08 20:59:57 +0800

    1

--- 3002.c
+++ 3002.c
@@ -1,59 +1,33 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-int max(int a, int b)
-{
-    return (a > b) ? a : b;
-}
-
-// 递归函数：计算当前玩家相对于对手的最大分差
-// 返回值: (当前玩家得分 - 对手得分)
-int maxDiff(int nums[], int left, int right)
-{
-    // 基本情况：如果只剩一个元素，拿走它
-    if (left == right)
-    {
-        return nums[left];
-    }
-
-    // 选项 1：拿走左边的元素
-    // 我的得分 = nums[left] - (对手在剩下部分能拿到的最大净胜分)
-    int pickLeft = nums[left] - maxDiff(nums, left + 1, right);
-
-    // 选项 2：拿走右边的元素
-    // 我的得分 = nums[right] - (对手在剩下部分能拿到的最大净胜分)
-    int pickRight = nums[right] - maxDiff(nums, left, right - 1);
-
-    // 作为最优策略，选择收益最大的一项
-    return max(pickLeft, pickRight);
-}
-
+#include <stdlib.h>
 int main()
 {
-    int n;
-    // 读取数组长度
-    if (scanf("%d", &n) != 1)
-        return 0;
-
-    // 使用C99标准的变长数组 (VLA)
-    int nums[n];
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &nums[i]);
-    }
-
-    // 计算玩家 1 的最终净胜分 (范围是 0 到 n-1)
-    int result = maxDiff(nums, 0, n - 1);
-
-    // 如果净胜分 >= 0，说明玩家 1 赢了或者平局
-    if (result >= 0)
-    {
-        printf("true\n");
-    }
-    else
-    {
-        printf("false\n");
-    }
-
     return 0;
+}
+// 辅助递归函数
+// i 是左边界，j 是右边界
+int net_score(int *nums, int i, int j)
+{
+    // 1. Base Case: 如果 i 和 j 撞上了 (只剩一个数)
+    if (i == j)
+    {
+        return nums[i];
+    }
+
+    // 2. 选择 A: 拿左边
+    // 这里的逻辑是：我拿的分 - (对手在剩下局面里比我多拿的分)
+    int pick_left = nums[i] - net_score(nums, i + 1, j);
+
+    // 3. 选择 B: 拿右边
+    int pick_right = nums[j] - net_score(nums, i, j - 1);
+
+    // 4. 返回两种选择里对我最有利的 (最大值)
+    return (pick_left > pick_right) ? pick_left : pick_right;
+}
+
+// 主函数
+bool PredictTheWinner(int *nums, int numsSize)
+{
+    // 如果 玩家1 的净胜分 >= 0，就赢了
+    return net_score(nums, 0, numsSize - 1) >= 0;
 }
